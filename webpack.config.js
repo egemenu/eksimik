7a2568c1959a5f07d760b89aebef83a81d168245
Egemen <mail@ege.im>
2021-09-13 19:22:58 +0200

    vulnerability fixes

--- webpack.config.js
+++ webpack.config.js
@@ -2,55 +2,56 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  watch: true,
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s?[ac]ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: 'css-loader', options: { url: false, sourceMap: true } },
-          { loader: 'sass-loader', options: { sourceMap: true } }
-        ],
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      cleanStaleWebpackAssets: !devMode,
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: "style.css"
-    }),
-    new CopyPlugin([
-      { from: 'src/public', to: '' },
-    ]),
-  ],
-  mode : devMode ? 'development' : 'production',
-  watch: devMode
-}
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s?[ac]ss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{loader: 'css-loader', options: {url: false, sourceMap: true}},
+					{loader: 'sass-loader', options: {sourceMap: true}},
+				],
+			},
+			{
+				enforce: 'pre',
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader',
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin({
+			cleanStaleWebpackAssets: !devMode,
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'style.css',
+		}),
+		new CopyPlugin({
+			patterns: [
+				{from: 'src/public', to: ''},
+			],
+		}),
+	],
+	mode: devMode ? 'development' : 'production',
+	watch: devMode,
+};
